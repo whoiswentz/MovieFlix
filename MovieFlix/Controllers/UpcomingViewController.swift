2fe15345e63e23d31216ff7982518b8ca06cc6a3
Vinicios Wentz <vinicios@wentz.io>
2023-08-09 15:47:09 -0300

    Creating the ViewControllers

---
+++ MovieFlix/Controllers/UpcomingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  UpcomingViewController.swift
+//  MovieFlix
+//
+//  Created by Vinicios Wentz on 09/08/23.
+//
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
