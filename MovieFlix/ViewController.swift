44d76731e955a07b591e8a5cff37e1ecdfc1edbe
Vinicios Wentz <vinicios@wentz.io>
2023-08-09 11:07:10 -0300

    Initial Commit

---
+++ MovieFlix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieFlix
+//
+//  Created by Vinicios Wentz on 09/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
