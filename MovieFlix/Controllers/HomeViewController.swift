6e4d5dc7d9a674e49ddc420f669781fa413e044c
Vinicios Wentz <vinicios@wentz.io>
2023-08-10 21:58:54 -0300

    feat: cell constraints setted up
    height for row at: 200
    height for header in section: 40
    
    Signed-off-by: Vinicios Wentz <vinicios@wentz.io>

--- MovieFlix/Controllers/HomeViewController.swift
+++ MovieFlix/Controllers/HomeViewController.swift
@@ -40,4 +40,12 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
         cell.textLabel?.text = "Hello World"
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
+    }
 }
